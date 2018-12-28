13484bf0c3c9f00465c7503203c479d519478721
Alex Eisele <eisele.alex@gmail.com>
2018-12-28 17:52:32 -0600

    Add puptime icon

--- packages/puptime-ui/src/components/AppBar.tsx
+++ packages/puptime-ui/src/components/AppBar.tsx
@@ -2,10 +2,14 @@ import * as React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
 
 export default () => (
   <AppBar position="static">
     <Toolbar>
+      <Avatar aria-label="PuptimeLogo" style={{ marginRight: "1rem" }}>
+        <img src="/pp-icon.png" height="42" width="42" />
+      </Avatar>
       <Typography variant="h5" color="inherit">
         Puptime - Fetch and Monitor Load
       </Typography>
